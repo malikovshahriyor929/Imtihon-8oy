c1b1ea54e3f44e506e47e8a38a25f2485473c837
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-05-18 20:03:30 +0500

    fixed bugs and card with responsive and fixed some error and rise performance

--- app/about/[id]/page.tsx
+++ app/about/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Posts from "@/components/customers";
 import ProductPage from "@/components/dynamic cart";
 import Products2 from "@/components/products2";
-const CardPage = async ({ params }: { params: { id: string } }) => {
+const CardPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   return (
     <>
