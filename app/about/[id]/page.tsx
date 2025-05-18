0ef25ea7df7d5c0353d22349073bdb9f6c2e65fc
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-05-18 22:45:59 +0500

    fixed bugs

--- app/about/[id]/page.tsx
+++ app/about/[id]/page.tsx
@@ -3,19 +3,14 @@ import Posts from "@/components/customers";
 import ProductPage from "@/components/dynamic cart";
 import Products2 from "@/components/products2";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-const Page = ({ params }: PageProps) => {
+export default async function Pag({ params }: { params: { id: string } }) {
+  const { id } = params;
   return (
     <div>
-      <ProductPage id={params.id} />
+      <ProductPage id={id} />
       <Posts />
       <Products2 />
     </div>
   );
-};
-export default Page;
+}
+// export default Page;
