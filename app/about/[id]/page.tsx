073c6a03c88b0316083bdd07ff97d0df81870e17
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-05-18 22:41:44 +0500

    fixed bugs

--- app/about/[id]/page.tsx
+++ app/about/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Posts from "@/components/customers";
 import ProductPage from "@/components/dynamic cart";
 import Products2 from "@/components/products2";
@@ -11,11 +11,11 @@ interface PageProps {
 
 const Page = ({ params }: PageProps) => {
   return (
-    <>
+    <div>
       <ProductPage id={params.id} />
       <Posts />
       <Products2 />
-    </>
+    </div>
   );
 };
 export default Page;
